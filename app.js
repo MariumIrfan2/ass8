b01e8ddfdc2434cbb27f59eb3a69308a5eac044d
MariumIrfan2 <111229052+MariumIrfan2@users.noreply.github.com>
2022-09-23 15:55:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,368 @@
+// ALERTS TASK START
+
+// TASK 1
+// alert("Welcome to my website");
+
+// TASK 2
+// alert("Error! Please enter a valid password.");
+
+// TASK 3
+// alert("Welcome to JS Land... \n Happy Coding!");
+
+// TASK 4
+//  alert("Welcome to JS Land...");
+// alert("Happy Coding");
+
+// TASK 5 (done in browsers console)
+// ...ALERTS TASK END...
+
+
+// ...Chp 2 START...
+// TASK 1
+// var username = "username";
+
+// TASK 2
+// var myName = "MArium Irfan";
+
+// TASK 3
+// var message = "Hello World";
+// alert(message);
+
+// TASK 4
+// var studentName = "Jhone Doe";
+// var studentAge = "15 years old";
+// var studentCourse = "Certified Mobile Application Development";
+
+// alert(studentName);
+// alert(studentAge);
+// alert(studentCourse);
+
+// TASK 5
+// var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP"
+// alert(pizza);
+
+// TASK 6
+// var email = "example@example.com"
+// alert("My email address is " + email)
+
+// TASK 7
+// var book = "A Smarter Way To Learn Javascript";
+// alert("I am trying to learnfrom a book " + book)
+
+// TASK 8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// TASK 9
+// var text = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(text);
+// document.write(text);
+
+// ...Chp 2 END...
+
+
+// ...Chp 3 START...
+
+// TASK 1
+// var age = 21;
+// alert("I am " + age + " years old");
+
+// TASK 2
+// var visits = "14";
+// alert("you have visited this site" + "times");
+
+// TASK 3
+// var birthYear = 2001;
+// document.write("My birth year is " + birthYear  +  "\n Data type of my declared variable is number");
+
+// TASK 4
+// var visitorName = "Jhone Doe";
+// var ProductTitle = "Tshirt(s)";
+// var Quantity = 5;
+
+// document.write(visitorName + " ordered " + Quantity + ProductTitle + " on XYZ Clothing store");
+
+// ...Chp 3 END...
+
+// ...Chp 4 START...
+
+// TASK 1
+var studentFirstName = "";
+// TASK 1
+// TASK 1
+// TASK 1
+// ...Chp 4 END...
+
+
+// ...Chp 5 START...
+// TASK 1
+// var x = 3;
+// var y = 5;
+
+// var sum = x + y ;
+
+// document.write("The sum of " + x + " and " + y + " is " + sum );
+
+// TASK 2
+// var x = 3;
+// var y = 5;
+
+// var sum = y - x ;
+
+// document.write("The subraction of " + y + " and " + x + " is " + sum );
+
+// var x = 3;
+// var y = 5;
+
+// var sum = y - x ;
+
+// document.write("The subraction of " + y + " and " + x + " is " + sum );
+
+// var x = 3;
+// var y = 5;
+
+// var sum = y * x ;
+
+// document.write("The multiplication of " + y + " and " + x + " is " + sum );
+
+// var x = 3;
+// var y = 5;
+
+// var sum = y / x ;
+
+// document.write("The division of " + y + " and " + x + " is " + sum );
+
+// TASK 3
+// var no = 12;
+// document.write("value after variable declaration is: " + no)
+
+// var num = 5;
+// document.write("Initial value: " + num);
+
+// var newNum = num++;
+// document.write("Value after increment is: " + num);
+
+// var newNum = num + 7;
+// document.write("Value after increment is: " + newNum);
+
+// var minus = --newNum;
+// document.write("Value after decrement is: " + minus);
+
+// var remaining = minus % 3;
+// document.write("the remainder is:" + remaining)
+
+// TASK 4
+// var ticketCost = 600;
+// var totalCost = 600 * 5;
+// document.write("Total cost to buy 5 tickets to a movie is " + totalCost + "PKR");
+
+// TASK 5
+// var table = 4;
+
+// document.write("Table of " + table + "<br>")
+
+// for (i = 1; i < 11; i++) {
+
+//     document.write(table + " X " + i + "=" + i * table + "<br>")
+// }
+
+// TASK 6
+// var C = 25;
+// var cToF = (C * 9 / 5) + 32;
+// document.write(C + "C is " + cToF + "F" + "<br>");
+
+// var F = 70;
+// var fToC = (F - 32) * 5 / 9;
+// document.write(F + "F is " + fToC + "F" + "<br>");
+
+// TASK 7
+// var priceOfItem1 = 650;
+// var priceOfItem2 = 100;
+// var quantityOfItem1 = 3;
+// var quantityOfItem2 = 7;
+// var shippingCharges = 100;
+
+// var totalCost = (priceOfItem1 * quantityOfItem1) + (priceOfItem2 * quantityOfItem2) + shippingCharges;
+
+// document.write("Price of item 1 is " + priceOfItem1 + "<br>");
+// document.write("Quantity of item 1 is " + quantityOfItem1 + "<br>");
+// document.write("Price of item 2 is " + priceOfItem2 + "<br>");
+// document.write("Quantity of item 2 is " + quantityOfItem2 + "<br>");
+// document.write("shipping Charges " + shippingCharges + "<br>" + "<br>" + "<br>");
+// document.write("Total cost of your order is " + totalCost);
+// var priceOfItem1 = 650;
+// var priceOfItem2 = 100;
+// var quantityOfItem1 = 3;
+// var quantityOfItem2 = 7;
+// var shippingCharges = 100;
+
+// var totalCost = (priceOfItem1 * quantityOfItem1) + (priceOfItem2 * quantityOfItem2) + shippingCharges;
+
+// document.write("Price of item 1 is " + priceOfItem1 + "<br>");
+// document.write("Quantity of item 1 is " + quantityOfItem1 + "<br>");
+// document.write("Price of item 2 is " + priceOfItem2 + "<br>");
+// document.write("Quantity of item 2 is " + quantityOfItem2 + "<br>");
+// document.write("shipping Charges " + shippingCharges + "<br>" + "<br>" + "<br>");
+// document.write("Total cost of your order is " + totalCost);
+// var priceOfItem1 = 650;
+// var priceOfItem2 = 100;
+// var quantityOfItem1 = 3;
+// var quantityOfItem2 = 7;
+// var shippingCharges = 100;
+
+// var totalCost = (priceOfItem1 * quantityOfItem1) + (priceOfItem2 * quantityOfItem2) + shippingCharges;
+
+// document.write("Price of item 1 is " + priceOfItem1 + "<br>");
+// document.write("Quantity of item 1 is " + quantityOfItem1 + "<br>");
+// document.write("Price of item 2 is " + priceOfItem2 + "<br>");
+// document.write("Quantity of item 2 is " + quantityOfItem2 + "<br>");
+// document.write("shipping Charges " + shippingCharges + "<br>" + "<br>" + "<br>");
+// document.write("Total cost of your order is " + totalCost);
+// var priceOfItem1 = 650;
+// var priceOfItem2 = 100;
+// var quantityOfItem1 = 3;
+// var quantityOfItem2 = 7;
+// var shippingCharges = 100;
+
+// var totalCost = (priceOfItem1 * quantityOfItem1) + (priceOfItem2 * quantityOfItem2) + shippingCharges;
+
+// document.write("Price of item 1 is " + priceOfItem1 + "<br>");
+// document.write("Quantity of item 1 is " + quantityOfItem1 + "<br>");
+// document.write("Price of item 2 is " + priceOfItem2 + "<br>");
+// document.write("Quantity of item 2 is " + quantityOfItem2 + "<br>");
+// document.write("shipping Charges " + shippingCharges + "<br>" + "<br>" + "<br>");
+// document.write("Total cost of your order is " + totalCost);
+// var priceOfItem1 = 650;
+// var priceOfItem2 = 100;
+// var quantityOfItem1 = 3;
+// var quantityOfItem2 = 7;
+// var shippingCharges = 100;
+
+// var totalCost = (priceOfItem1 * quantityOfItem1) + (priceOfItem2 * quantityOfItem2) + shippingCharges;
+
+// document.write("Price of item 1 is " + priceOfItem1 + "<br>");
+// document.write("Quantity of item 1 is " + quantityOfItem1 + "<br>");
+// document.write("Price of item 2 is " + priceOfItem2 + "<br>");
+// document.write("Quantity of item 2 is " + quantityOfItem2 + "<br>");
+// document.write("shipping Charges " + shippingCharges + "<br>" + "<br>" + "<br>");
+// document.write("Total cost of your order is " + totalCost);
+// var priceOfItem1 = 650;
+// var priceOfItem2 = 100;
+// var quantityOfItem1 = 3;
+// var quantityOfItem2 = 7;
+// var shippingCharges = 100;
+
+// var totalCost = (priceOfItem1 * quantityOfItem1) + (priceOfItem2 * quantityOfItem2) + shippingCharges;
+
+// document.write("Price of item 1 is " + priceOfItem1 + "<br>");
+// document.write("Quantity of item 1 is " + quantityOfItem1 + "<br>");
+// document.write("Price of item 2 is " + priceOfItem2 + "<br>");
+// document.write("Quantity of item 2 is " + quantityOfItem2 + "<br>");
+// document.write("shipping Charges " + shippingCharges + "<br>" + "<br>" + "<br>");
+// document.write("Total cost of your order is " + totalCost);
+// var priceOfItem1 = 650;
+// var priceOfItem2 = 100;
+// var quantityOfItem1 = 3;
+// var quantityOfItem2 = 7;
+// var shippingCharges = 100;
+
+// var totalCost = (priceOfItem1 * quantityOfItem1) + (priceOfItem2 * quantityOfItem2) + shippingCharges;
+
+// document.write("Price of item 1 is " + priceOfItem1 + "<br>");
+// document.write("Quantity of item 1 is " + quantityOfItem1 + "<br>");
+// document.write("Price of item 2 is " + priceOfItem2 + "<br>");
+// document.write("Quantity of item 2 is " + quantityOfItem2 + "<br>");
+// document.write("shipping Charges " + shippingCharges + "<br>" + "<br>" + "<br>");
+// document.write("Total cost of your order is " + totalCost);
+// var priceOfItem1 = 650;
+// var priceOfItem2 = 100;
+// var quantityOfItem1 = 3;
+// var quantityOfItem2 = 7;
+// var shippingCharges = 100;
+
+// var totalCost = (priceOfItem1 * quantityOfItem1) + (priceOfItem2 * quantityOfItem2) + shippingCharges;
+
+// document.write("Price of item 1 is " + priceOfItem1 + "<br>");
+// document.write("Quantity of item 1 is " + quantityOfItem1 + "<br>");
+// document.write("Price of item 2 is " + priceOfItem2 + "<br>");
+// document.write("Quantity of item 2 is " + quantityOfItem2 + "<br>");
+// document.write("shipping Charges " + shippingCharges + "<br>" + "<br>" + "<br>");
+// document.write("Total cost of your order is " + totalCost);
+// var priceOfItem1 = 650;
+// var priceOfItem2 = 100;
+// var quantityOfItem1 = 3;
+// var quantityOfItem2 = 7;
+// var shippingCharges = 100;
+
+// var totalCost = (priceOfItem1 * quantityOfItem1) + (priceOfItem2 * quantityOfItem2) + shippingCharges;
+
+// document.write("Price of item 1 is " + priceOfItem1 + "<br>");
+// document.write("Quantity of item 1 is " + quantityOfItem1 + "<br>");
+// document.write("Price of item 2 is " + priceOfItem2 + "<br>");
+// document.write("Quantity of item 2 is " + quantityOfItem2 + "<br>");
+// document.write("shipping Charges " + shippingCharges + "<br>" + "<br>" + "<br>");
+// document.write("Total cost of your order is " + totalCost);
+
+//  TaSK 8
+
+// var totalMArks = 980;
+// var marksObtained = 804;
+// var percentage = (804 / 980) * 100;
+
+// document.write("Total Marks: " + totalMArks + "<br>");
+// document.write("Marks Obtained: " + marksObtained + "<br>");
+// document.write("Precentage: " + percentage + "%" + "<br>");
+
+// TASK 9
+// var dollars = 10;
+// var riyals = 25;
+// var totalPkr = (10 * 104.80) + (28 * 25);
+// document.write("Currency In PKR" + "<br>" + "<br>" + "<br>" + "<br>");
+// document.write("Total currency in PKR: " + totalPkr);
+
+// TASK 10
+// var x = 5;
+// var calculation = (x + 5) * 10 / 2;
+// console.log(calculation);
+
+// TASK 11
+// var currentYear = 2016;
+// var birthYear = 1992;
+// var age = currentYear - birthYear
+
+// document.write("The Age Calculator"+ "<br>");
+
+// document.write("current Year:" + currentYear + "<br>");
+// document.write("Birth Year:" + birthYear + "<br>");
+// document.write("Your Age:" + age + "<br>");
+
+// TASK 12
+// var radius = 20;
+// var circumfrence = 2 * 3.142 * radius;
+// var area = 3.142 * radius * radius;
+
+// document.write("Radius of a circle: " + radius + "<br>");
+// document.write("Circumfrence of a circle: " + circumfrence + "<br>");
+// document.write(" Area of a circle: " + area + "<br>");
+
+// TASK 13
+// var fSnack = "Choclate Chips";
+// var currentAge = 15;
+// var maxAge = 65;
+// var amtPerDay = 3;
+// var toatalReq = (65 - 15) * 3;
+
+
+// document.write("The Lifetime Supply Calculator" + "<br>" + "<br>" + "<br>" + "<br>");
+
+// document.write("Favourite Snack: " + fSnack + "<br>")
+// document.write("Current Age: " + currentAge + "<br>")
+// document.write("Estimated MAximum Age: " + maxAge + "<br>")
+// document.write("Amount of snacks per Day: " + +"<br>")
+// document.write("You will need " + toatalReq + " to last you until the ripe old age of " + maxAge + "<br>");
+// ...Chp 5 END...
+
+
+
+
+
+
